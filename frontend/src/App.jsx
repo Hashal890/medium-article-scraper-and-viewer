4425951758b5b8c37081e29c287b262ac4b0bc24
amolpradeshi <pardeshiharshal90@gmail.com>
2024-07-24 14:05:32 +0530

    created app template

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box bg={"#1A202C"} minH={"100vh"}>
+      Medium Article Scraper and Viewer
+    </Box>
+  );
+}
+
+export default App;
