88c0e23953434c6f4b90107c8e16d74b0fff2eb3
amolpradeshi <pardeshiharshal90@gmail.com>
2024-07-25 19:39:49 +0530

    added new env variables

--- backend/controller/scrape.controller.js
+++ backend/controller/scrape.controller.js
@@ -3,7 +3,11 @@ const puppeteer = require("puppeteer");
 const scrapeMediumArticles = async (topic) => {
   let browser;
   try {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      executablePath:
+        process.env.PUPPETEER_EXECUTABLE_PATH,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.goto(`https://medium.com/search?q=${topic}`, {
       waitUntil: "networkidle2",
